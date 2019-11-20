b5f307c86807b06240f0795e6ba871afbd8f4061
aji <ajifadillahramah@gmail.com>
2019-11-20 19:16:15 +0700

    upload pertemuan17

---
+++ tugas/Pertemuan17/blog_website/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+// controller
+var application_controller = require('../controllers/applicationController.js');
+
+// routes
+router.get('/', application_controller.home);
+router.get('/about', application_controller.about);
+router.get('/contact', application_controller.contact);
+
+module.exports = router;
